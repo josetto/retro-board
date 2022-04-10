d3124f2b1aff5b29814897e0d97f4cc69fd5ce65
Josset Garcia <josset.garcia1@gmail.com>
2022-04-10 18:28:57 -0400

    add annotations to enable jpa auditing

---
+++ src/main/java/cl/josetto/retroboard/config/RepositoryConfig.java
@@ -0,0 +1,9 @@
+package cl.josetto.retroboard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class RepositoryConfig {
+}
--- src/main/java/cl/josetto/retroboard/model/Comment.java
+++ src/main/java/cl/josetto/retroboard/model/Comment.java
@@ -2,25 +2,38 @@ package cl.josetto.retroboard.model;
 
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.yaml.snakeyaml.comments.CommentType;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Comment {
     @Id
     @GeneratedValue
     private Long id;
 
-    @Enumerated(EnumType.STRING)
     private String comment;
+
+    @Enumerated(EnumType.STRING)
+    private CommentType commentType;
+
+    @CreatedDate
+    private Timestamp createdDate;
+
+    @CreatedBy
+    private String createdBy;
 
     public Comment() {
     }
 
-    public Comment(Long id, String comment) {
+    public Comment(Long id, String comment, Timestamp createdDate, String createdBy) {
         this.id = id;
         this.comment = comment;
+        this.createdDate = createdDate;
+        this.createdBy = createdBy;
     }
 
     public Long getId() {
@@ -39,5 +52,27 @@ public class Comment {
         this.comment = comment;
     }
 
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
 
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public CommentType getCommentType() {
+        return commentType;
+    }
+
+    public void setCommentType(CommentType commentType) {
+        this.commentType = commentType;
+    }
 }
